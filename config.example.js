f9412825e2fa8bd5aeb9980dd58e4f98a739c221
kenchen <independentchen@gmail.com>
2020-03-16 21:09:46 +0800

    Finish mysql configuration and migrations

---
+++ config.example.js
@@ -0,0 +1,12 @@
+const dbConfig = {
+  host: "127.0.0.1",
+  user: "root",
+  password: "qwnmTANK2006#",
+  database: "node_message",
+  charset: "utf8",
+  timezone: "Z"
+};
+
+module.exports = {
+  dbConfig
+};
